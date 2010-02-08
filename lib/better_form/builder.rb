493009711a2b7cbef65f6f1a36bbc002a51cea2c
Nicholas Firth-McCoy <nfm@nfm.id.au>
2010-02-08 15:22:13 +1100

    Defined BetterForm::InstanceTag. Concatenated the label and text field tag and made the label text human readable

--- lib/better_form/builder.rb
+++ lib/better_form/builder.rb
@@ -1,8 +1,13 @@
 module BetterForm
 	class Builder < ActionView::Helpers::FormBuilder
 		def text_field(object_name, method, options = {})
-			InstanceTag.new(object_name, method, self, options.delete(:object)).to_label_tag("#{method}:", options)
-			InstanceTag.new(object_name, method, self, options.delete(:object)).to_input_field_tag("text", options)
+			human_readable_method = method.to_s.gsub(/_/, " ").capitalize
+			text_field = InstanceTag.new(object_name, method, self, options.delete(:object)).to_label_tag("#{human_readable_method}:", options)
+			label = InstanceTag.new(object_name, method, self, options.delete(:object)).to_input_field_tag("text", options)
+			return text_field + label
 		end
+	end
+
+	class InstanceTag < ActionView::Helpers::InstanceTag
 	end
 end
