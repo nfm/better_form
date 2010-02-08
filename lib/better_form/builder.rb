509459aa5d61205cf913110e9a1c29f04ab6038f
Nicholas Firth-McCoy <nfm@nfm.id.au>
2010-02-09 09:28:41 +1100

    Redefined text_field as per FormBuilder's initialization of text_field within the context of a model (line 942 in form_helper.rb)

--- lib/better_form/builder.rb
+++ lib/better_form/builder.rb
@@ -2,11 +2,11 @@ module BetterForm
 	class Builder < ActionView::Helpers::FormBuilder
 		include ActionView::Helpers::TagHelper
 
-		def text_field(object_name, method, options = {})
+		def text_field(method, options = {})
 			human_readable_method = method.to_s.gsub(/_/, " ").capitalize
 			options[:class] = "better_text_field #{options[:class]}"
 			options[:value] = human_readable_method
-			InstanceTag.new(object_name, method, self, options.delete(:object)).to_input_field_tag("text", options) + tag('br')
+			InstanceTag.new(@object_name, method, self, options.delete(:object)).to_input_field_tag("text", options) + tag('br')
 		end
 	end
 
