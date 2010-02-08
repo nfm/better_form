1660fc00c6bd0f0cce0e91a09bbedb6a416177c9
Nicholas Firth-McCoy <nfm@nfm.id.au>
2010-02-08 17:47:53 +1100

    Created inital better_form.js in init.rb. Used init.rb to create the file in public/javascripts/

--- install.rb
+++ install.rb
@@ -1,1 +1,27 @@
-# Install hook code here
+# Install better_form.js to public/javascripts/
+path = "public/javascripts/better_form.js"
+puts "Generating #{path}"
+File.open("#{Rails.root}/#{path}", "w") do |file|
+  file.print <<-EOS
+$(function() {
+  var initialValue = '';
+
+  $('input.better_text_field').focus(function() {
+    /* If this field has not been completed */
+    if (!$(this).hasClass('better_completed_field')) {
+      initialValue = this.value;
+      this.value = '';
+    }
+  });
+
+  $('input.better_text_field').blur(function() {
+    /* Reset the fields initial value if it wasn't filled it */
+    if (this.value == '') {
+      this.value = initialValue;
+    } else {
+      $(this).addClass('better_completed_field');
+    }
+  });
+});
+EOS
+end
