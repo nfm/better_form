a9e3101c0ad8843e893f1e6c908cc46b7c752474
Nicholas Firth-McCoy <nfm@nfm.id.au>
2010-02-09 12:25:06 +1100

    Added .better_validated_field to fields with :validated => true

--- lib/better_form/builder.rb
+++ lib/better_form/builder.rb
@@ -7,10 +7,16 @@ module BetterForm
 			options[:class] = "better_text_field #{options[:class]}"
 			options[:value] = human_readable_method
 			options[:title] = human_readable_method
+
 			if options.delete(:required)
 				options[:class] = "#{options[:class]} better_required_field"
 				required_span = content_tag_string(:span, "&nbsp;&#42;", { :class => 'better_required_field' })
 			end
+
+			if options.delete(:validated)
+				options[:class] = "#{options[:class]} better_validated_field"
+			end
+
 			InstanceTag.new(@object_name, method, self, options.delete(:object)).to_input_field_tag("text", options) + (required_span ||= '') + tag('br')
 		end
 
