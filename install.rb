4277d7e7e07ab8291cd1d7b1584b425e1c934bff
Nicholas Firth-McCoy <nfm@nfm.id.au>
2010-02-10 10:14:44 +1100

    Watched inputs with .better_validated_field class and ajax validated them on blur event

--- install.rb
+++ install.rb
@@ -6,19 +6,26 @@ File.open("#{Rails.root}/#{path}", "w") do |file|
 $(function() {
   var initialValue = '';
 
+  // Store the initial value and clear the input
   $('input.better_text_field').focus(function() {
-		initialValue = this.value;
-		this.value = '';
+    initialValue = this.value;
+    this.value = '';
   });
 
   $('input.better_text_field').blur(function() {
-    /* Reset the fields initial value if it is required and wasn't filled it */
+    // Restore the initial value if the field is required and no value was entered
     if ($(this).hasClass('better_required_field') && (this.value == '')) {
       this.value = initialValue;
+    // Else mark the field as completed
     } else {
       $(this).addClass('better_completed_field');
     }
   });
+
+  $('input.better_validated_field').blur(function() {
+    $.ajax({data:'authenticity_token=' + encodeURIComponent('iH4oAzNSO8OUiyFjVksfIdjEzmiWbL5BfM3mgp4rws4=')+ '&' + this.name + '=' + this.value + '&field_id=' + this.id, dataType:'script', type:'post', url: 'ajax_validate_' + this.name});
+  });
+
 });
 EOS
 end
