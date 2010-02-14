69175bb2f4637eb37db01433d6d3a6d63622a5ff
Nicholas Firth-McCoy <nfm@nfm.id.au>
2010-02-14 11:48:51 +1100

    Removed file better_form.js when uninstalling plugin

--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,9 @@
-# Uninstall hook code here
+# Remove better_form.js from public/javascripts/
+path = "public/javascripts/better_form.js"
+puts "Removing #{path}"
+begin
+	File.remove(path)
+rescue Exception => e
+	puts "Unable to remove #{path}"
+	puts e
+end
