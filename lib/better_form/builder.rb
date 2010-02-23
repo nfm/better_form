48d242a1619773c4b43cb708e6cae5abfa74b7b8
Nicholas Firth-McCoy <nfm@nfm.id.au>
2010-02-24 10:30:07 +1100

    Defined check_box method to handle checkboxes. Wrote generate_* helper methods to DRY up label/description/required_span creation

--- lib/better_form/builder.rb
+++ lib/better_form/builder.rb
@@ -60,12 +60,54 @@ module BetterForm
 			content_tag_string(:p, (label ||= '') + InstanceTag.new(@object_name, method, self, options.delete(:object)).to_select_tag(choices, options, html_options) + (required_span ||= '') + tag('br') + (description_span ||= ''), { :class => 'better_field'})
 		end
 
+		def check_box(method, options = {}, checked_value = "1", unchecked_value = "0") 
+			human_readable_method = generate_human_readable_method(method)
+			options[:class] = "better_checkbox_field #{options[:class]}"
+			options[:title] = human_readable_method
+			required = options.delete(:required)
+			validated = options.delete(:validated)
+			labelled = options.delete(:labelled)
+			required_span = label_tag = description_span = ''
+
+			if required == true || (required != false && @template.require_all?)
+				options[:class] = "#{options[:class]} better_required_field"
+				required_span = generate_required_span
+			end
+
+			if labelled == true || (labelled != false && @template.label_all?)
+				label_tag = generate_label(method, human_readable_method)
+			end
+
+			if description = options.delete(:description)
+				description_span = generate_description(description)
+			end
+
+			content_tag_string(:p, InstanceTag.new(@object_name, method, self, options.delete(:object)).to_check_box_tag(options, checked_value, unchecked_value) + label_tag + required_span + tag('br') + description_span, { :class => 'better_field'})
+		end
+
 		def submit(value = '', options = {})
 			options[:disabled] = true unless options[:disabled] == false
 			if value.blank?
 				value = (@object.new_record? ? "Create #{@object_name}" : "Save changes")
 			end
 			super(value, options)
+		end
+
+private
+		def generate_human_readable_method(method)
+			method.to_s.gsub(/_/, " ").capitalize
+		end
+
+		def generate_required_span
+			content_tag_string(:span, "*", { :class => 'better_required_field' })
+		end
+
+		def generate_label(method, name)
+			label(method, name, :class => 'better_label')
+		end
+
+		def generate_description(description)
+			content_tag_string(:span, description, { :class => 'better_described_field' })
 		end
 	end
 
