6d73bc6a61bd0a59807bc4fa6304d100c7075a39
Nicholas Firth-McCoy <nfm@nfm.id.au>
2010-03-15 15:15:42 +1100

    Changed rails root variable in install.rb

--- install.rb
+++ install.rb
@@ -1,9 +1,9 @@
-installed_files_dir = "#{Rails.root}/vendor/plugins/better_form/lib/installed_files"
+installed_files_dir = "#{RAILS_ROOT}/vendor/plugins/better_form/lib/installed_files"
 install_file "better_form.js", :to => "public/javascripts/better_form.js"
 install_file "better_form_initializer.rb", :to => "config/initializers/better_form.rb"
 install_file "style.css", :to => "public/stylesheets/better_form.css"
 
 def install_file(file, options = {})
 	puts "Generating #{options[:to]}"
-	FileUtils.copy("#{installed_files_dir}/#{file}", "#{Rails.root}/#{options[:to]}")
+	FileUtils.copy("#{installed_files_dir}/#{file}", "#{RAILS_ROOT}/#{options[:to]}")
 end
