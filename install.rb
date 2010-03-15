041a32f71d5c449c09a7c4268f1e7128a48b5f16
Nicholas Firth-McCoy <nfm@nfm.id.au>
2010-03-15 15:17:13 +1100

    Commented out install.rb to try to fix 'Plugin not found' bug

--- install.rb
+++ install.rb
@@ -1,3 +1,4 @@
+=begin
 installed_files_dir = "#{RAILS_ROOT}/vendor/plugins/better_form/lib/installed_files"
 install_file "better_form.js", :to => "public/javascripts/better_form.js"
 install_file "better_form_initializer.rb", :to => "config/initializers/better_form.rb"
@@ -7,3 +8,4 @@ def install_file(file, options = {})
 	puts "Generating #{options[:to]}"
 	FileUtils.copy("#{installed_files_dir}/#{file}", "#{RAILS_ROOT}/#{options[:to]}")
 end
+=end
