1d5874f575339bde5654cb579788b8e5bd3a3c91
Nicholas Firth-McCoy <nfm@nfm.id.au>
2010-03-15 15:35:17 +1100

    Fixed 'Plugin not found' bug caused by improperly initialized variable in install.rb

--- install.rb
+++ install.rb
@@ -1,11 +1,8 @@
-puts installed_files_dir = "#{RAILS_ROOT}/vendor/plugins/better_form/lib/installed_files"
-=begin
 install_file "better_form.js", :to => "public/javascripts/better_form.js"
 install_file "better_form_initializer.rb", :to => "config/initializers/better_form.rb"
 install_file "style.css", :to => "public/stylesheets/better_form.css"
 
 def install_file(file, options = {})
 	puts "Generating #{options[:to]}"
-	FileUtils.copy("#{installed_files_dir}/#{file}", "#{RAILS_ROOT}/#{options[:to]}")
+	FileUtils.copy("#{File.dirname(__FILE__)}/lib/installed_files/#{file}", "#{RAILS_ROOT}/#{options[:to]}")
 end
-=end
