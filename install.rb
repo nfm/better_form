204b37238f6a437c9f423ab5b66b1760e1088d73
Nicholas Firth-McCoy <nfm@nfm.id.au>
2010-03-15 16:04:52 +1100

    Added missing parentheses in install.rb

--- install.rb
+++ install.rb
@@ -1,8 +1,8 @@
 puts "Copying files..."
 
 src_dir = File.join(File.dirname(__FILE__), "lib", "installed_files")
-FileUtils.copy(File.join(src_dir, "better_form_initializer.rb", "#{RAILS_ROOT}/config/initializers/better_form_initializer.rb")
-FileUtils.copy(File.join(src_dir, "better_form.js", "#{RAILS_ROOT}/public/javascripts/better_form.js")
-FileUtils.copy(File.join(src_dir, "better_form.css", "#{RAILS_ROOT}/public/stylesheets/better_form.css")
+FileUtils.copy(File.join(src_dir, "better_form_initializer.rb"), "#{RAILS_ROOT}/config/initializers/better_form_initializer.rb")
+FileUtils.copy(File.join(src_dir, "better_form.js"), "#{RAILS_ROOT}/public/javascripts/better_form.js")
+FileUtils.copy(File.join(src_dir, "better_form.css"), "#{RAILS_ROOT}/public/stylesheets/better_form.css")
 
 puts "Files copied - Installation complete!"
