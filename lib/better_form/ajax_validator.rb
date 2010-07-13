13ae11d28eb9add902b8429ae3a16b6bb906e001
Nicholas Firth-McCoy <nfm@nfm.id.au>
2010-07-13 22:37:36 +1000

    Rewrote ajax_validates_for method in ajax_validator.rb to create validation methods for nested attributes

--- lib/better_form/ajax_validator.rb
+++ lib/better_form/ajax_validator.rb
@@ -8,10 +8,11 @@ module BetterForm
 
 		module ClassMethods
 			def ajax_validates_for(model, options = {})
+				klass = model.to_s.camelize.constantize
+				object = klass.new
+
 				# Unless options[:include_default_attributes] was explicitly assigned => false
 				unless options.delete(:include_default_attributes) == false
-					klass = model.to_s.camelize.constantize
-					object = klass.new
 					ajax_attributes = object.attributes.symbolize_keys.keys
 				else
 					ajax_attributes = []
@@ -31,18 +32,26 @@ module BetterForm
 
 				# For each attribute to ajax validate
 				ajax_attributes.each do |attribute|
-					# Define a method 'ajax_validates_for_X'
-					define_method("ajax_validate_#{model}_#{attribute}") do
-						render :update do |page|
-							# If the model is valid on the given attribute
-							if (errors = klass.better_valid_on?(params[model])).blank?
-								# Mark the field as valid
-								page << "markFieldValid('#{params[:field_id]}');"
-							else
-								# Mark the field as invalid
-								page << "markFieldInvalid('#{params[:field_id]}', '#{escape_javascript(errors[0].to_s)}');"
-							end
-						end
+					define_ajax_validate_attribute_method(klass, model, attribute)
+				end
+
+				# For each model that this model accepts_nested_attributes_for
+				nested_attributes_models = []
+				nested_attributes_models = object.nested_attributes_options.keys
+				nested_attributes_models.each do |nested_model|
+					# Initialize an object of this nested_model
+					klass = nested_model.to_s.camelize.constantize
+					object = klass.new
+
+					# Extract the object's attributes
+					ajax_attributes = []
+					ajax_attributes = object.attributes.symbolize_keys.keys
+
+					# Define an ajax validation method for each of the object's attributes
+					ajax_attributes.each do |attribute|
+						# Concatenate model and nested_model so that the method name we define matches
+						# the dom id generated for the field and used for the URL to validate at
+						define_ajax_validate_attribute_method(klass, "#{model}_#{nested_model}", attribute)
 					end
 				end
 
@@ -60,6 +69,23 @@ module BetterForm
 							errors.each do |attr, error|
 								page << "markFieldInvalid('#{model}_#{error[0].instance_variable_get('@attribute')}', '#{escape_javascript(error[0].to_s)}');"
 							end
+						end
+					end
+				end
+			end
+
+		private
+			def define_ajax_validate_attribute_method(klass, model, attribute)
+				# Define a method 'ajax_validates_for_X'
+				define_method("ajax_validate_#{model}_#{attribute}") do
+					render :update do |page|
+						# If the model is valid on the given attribute
+						if (errors = klass.better_valid_on?(params[model])).blank?
+							# Mark the field as valid
+							page << "markFieldValid('#{params[:field_id]}');"
+						else
+							# Mark the field as invalid
+							page << "markFieldInvalid('#{params[:field_id]}', '#{escape_javascript(errors[0].to_s)}');"
 						end
 					end
 				end
